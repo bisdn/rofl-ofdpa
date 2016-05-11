83ffd76b07ed8aeb5ad01b59fd33a49c77266cdd
Tobias Jungel <tobias.jungel@bisdn.de>
2016-05-11 18:12:38 +0200

    disable vlan on egress port
    adding disable_port_vid_egress for filtered egress groups and
    disable_port_unfiltered_egress for unfiltered egress groups

--- include/rofl/ofdpa/rofl_ofdpa_fm_driver.hpp
+++ include/rofl/ofdpa/rofl_ofdpa_fm_driver.hpp
@@ -23,8 +23,11 @@ public:
 
   uint32_t enable_port_vid_egress(const std::string &port_name, uint16_t vid,
                                   bool untagged = false);
+  uint32_t disable_port_vid_egress(const std::string &port_name, uint16_t vid,
+                                   bool untagged);
 
   uint32_t enable_port_unfiltered_egress(const std::string &port_name);
+  uint32_t disable_port_unfiltered_egress(const std::string &port_name);
 
   uint32_t enable_group_l2_multicast(uint16_t vid, uint16_t id,
                                      const std::list<uint32_t> &l2_interfaces,
