b70d406e4d8ce4539e5d934ed143516a3969b084
Andreas Koepsel <andreas.koepsel@bisdn.de>
2018-02-20 20:23:34 +0100

    rofl_ofdpa_fm_driver::enable_policy_l2() - adding default value to argument max_len

--- include/rofl/ofdpa/rofl_ofdpa_fm_driver.hpp
+++ include/rofl/ofdpa/rofl_ofdpa_fm_driver.hpp
@@ -40,6 +40,7 @@ namespace rofl {
 namespace openflow {
 
 class rofl_ofdpa_fm_driver final {
+  static const uint16_t DEFAULT_MAX_LEN = 1522;
 public:
   rofl_ofdpa_fm_driver();
   ~rofl_ofdpa_fm_driver();
@@ -118,7 +119,7 @@ public:
   cofflowmod enable_policy_arp(uint8_t ofp_version, bool update = false);
 
   cofflowmod enable_policy_l2(uint8_t ofp_version, const rofl::caddress_ll &mac,
-                              const uint16_t type, const uint16_t max_len);
+                              const uint16_t type, const uint16_t max_len = DEFAULT_MAX_LEN);
 
   cofflowmod enable_policy_specific_lacp(uint8_t ofp_version,
                                          const caddress_ll &eth_src,
